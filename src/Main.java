4e1c54e8c0d4bee98580b020ac025b518d4c3b45
Marcel Roquet <17marcelroquet@gmail.com>
2025-01-04 19:55:47 +0100

    creacio metodes pricipals i de la instancia 'banc'

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,46 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.util.Scanner;
 public class Main {
-    Scanner input=new Scanner(System.in);
+    private Scanner input =new Scanner(System.in);
     public  static void main(String[] args) {
+        Main Banc = new Main();
+
+    }
+    public boolean teCompte(){
+
+        return false;
+    }
+
+
+    public void crearCompte() {
+
+    }
+
+    public boolean verificarCredencials(){
+       return false;
+    }
+
+    public void mostrarBenvinguda(){
+
+    }
+
+    public void mostrarMenu(){
+
+    }
+
+    public void dadesPersonals(){
+
+    }
+
+    public void ingressarDiners(){
+
+    }
+
+    public void retirarDiners(){
+
+    }
+
+    public void mostrarMenu2(){
 
     }
 }
