be1a47bf7ef00be3eb5a5c47aadde03822910cd9
Marcel Roquet <17marcelroquet@gmail.com>
2025-01-04 20:15:44 +0100

    comprovaci0 de si te compte o no

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 import java.util.Scanner;
 public class Main {
     private Scanner input =new Scanner(System.in);
     public  static void main(String[] args) {
-        Main Banc = new Main();
+        Main Banc = new Main();          //creacio instancia
+        if (Banc.teCompte()){     // comprueba si tiene cuenta o no y gracias el metodo teCompte y dependiendo del resultado hace una cosa u otra
+
+        } else {
+
+        }
 
     }
-    public boolean teCompte(){
+    public boolean teCompte() {
+        String compte;
+        do {                            //bucle hasta que nos de una respuesta posible
+            System.out.println("Vols inicia sessió (I) o registrarte (R)");
+            compte = input.nextLine().toUpperCase();  //leemos input y lo pasamos a maysuculas
+        } while (compte.equals("R") || compte.equals("I"));
 
-        return false;
+        if (compte.equals("R")) {   // Si le da A Regitrarse enviaremos un False
+            return false;
+        } else {
+            return true;
+        }
     }
 
 
@@ -18,7 +31,7 @@ public class Main {
     }
 
     public boolean verificarCredencials(){
-       return false;
+        return false;
     }
 
     public void mostrarBenvinguda(){
